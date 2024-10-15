22974e7601d63d2b4d4bb23733efdaab3ee9fed0
hampus-code <144155634+hampus-code@users.noreply.github.com>
2024-10-15 12:19:48 +0200

    Feature: Added HampusTestView (#1)
    * Added HampusTestView
    
    * Added a new text
    
    * Add another text

---
+++ AdvancedGitPracticeForProject2/HampusTestView.swift
@@ -0,0 +1,20 @@
+//
+//  HampusTestView.swift
+//  AdvancedGitPracticeForProject2
+//
+//  Created by Hampus Andersson on 2024-10-15.
+//
+
+import SwiftUI
+
+struct HampusTestView: View {
+    var body: some View {
+        Text("This is Hampus Test View")
+        Text("A new text row")
+        Text("Another text row")
+    }
+}
+
+#Preview {
+    HampusTestView()
+}
