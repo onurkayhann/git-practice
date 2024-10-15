fd750b6e6b54d1487a9bde7b56ee5040af562291
hampus-code <hampusss96@gmail.com>
2024-10-15 13:14:12 +0200

    Feature: Add conflict text in ContentView

--- AdvancedGitPracticeForProject2/ContentView.swift
+++ AdvancedGitPracticeForProject2/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Git Practice")
+            Text("Hampus Conflict Text")
         }
         .padding()
     }
