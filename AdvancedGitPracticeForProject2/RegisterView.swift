67bab4e7a9eeb988ff0722912aada4afff8035cf
hampus-code <hampusss96@gmail.com>
2024-10-15 12:33:46 +0200

    Add RegisterView

---
+++ AdvancedGitPracticeForProject2/RegisterView.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterView.swift
+//  AdvancedGitPracticeForProject2
+//
+//  Created by Hampus Andersson on 2024-10-15.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text("Register View")
+    }
+}
+
+#Preview {
+    RegisterView()
+}
