0ec1b494322dc9285e6bcc3745457c82a14cbd5b
Onur <onur.kayhan90@gmail.com>
2024-10-15 10:59:23 +0200

    Initial Commit

---
+++ AdvancedGitPracticeForProject2/AdvancedGitPracticeForProject2App.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedGitPracticeForProject2App.swift
+//  AdvancedGitPracticeForProject2
+//
+//  Created by Onur Kayhan on 2024-10-15.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedGitPracticeForProject2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdvancedGitPracticeForProject2Tests/AdvancedGitPracticeForProject2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedGitPracticeForProject2Tests.swift
+//  AdvancedGitPracticeForProject2Tests
+//
+//  Created by Onur Kayhan on 2024-10-15.
+//
+
+import Testing
+@testable import AdvancedGitPracticeForProject2
+
+struct AdvancedGitPracticeForProject2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
