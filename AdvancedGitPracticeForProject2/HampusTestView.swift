0c9a85ce3f40a799f381d24925edee27247c6beb
hampus-code <hampusss96@gmail.com>
2024-10-15 12:13:21 +0200

    Added HampusTestView

---
+++ AdvancedGitPracticeForProject2/HampusTestView.swift
@@ -0,0 +1,18 @@
+//
+//  HampusTestView.swift
+//  AdvancedGitPracticeForProject2
+//
+//  Created by Hampus Andersson on 2024-10-15.
+//
+
+import SwiftUI
+
+struct HampusTestView: View {
+    var body: some View {
+        Text("This is Hampus Test View")
+    }
+}
+
+#Preview {
+    HampusTestView()
+}
