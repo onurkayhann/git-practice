cf6f160125a9454ccaf60d5823d4107812555005
Onur <onur.kayhan90@gmail.com>
2024-10-15 12:13:10 +0200

    add Onur

---
+++ AdvancedGitPracticeForProject2/Onur.swift
@@ -0,0 +1,18 @@
+//
+//  Onur.swift
+//  AdvancedGitPracticeForProject2
+//
+//  Created by Onur Kayhan on 2024-10-15.
+//
+
+import SwiftUI
+
+struct Onur: View {
+    var body: some View {
+        Text("Hello, Onur!")
+    }
+}
+
+#Preview {
+    Onur()
+}
